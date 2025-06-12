37bd2f81fc2b466d7ef3ed8250a519a10621ed26
andresfelipe-ui <andresfeliperodriguezprimicier@gmail.com>
2025-06-11 20:40:35 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,39 +1,12 @@
-# ****ZONA DE CLASE*****
-#se crea la clase
-class cliente:
-    #se crea el metodo constructo de la clase
-    def __int__(self):
-        #se crean los atributos de la clase
-        self.nombre_cliente = ""
-        self.apellido_cliente = ""
-        
-    #crear metodos get y set por atributos
-    def get_nombre(self):
-        return self.nombre_cliente
-    
-    def get_apellido(self):
-        return self.apellido_cliente
-    
-    def set_nombre(self, dato_nombre):
-        self.nombre_cliente = dato_nombre
-        
-    def set_apellido(self, dato_apellido):
-        self.apellido_cliente = dato_apellido
-        
-            
-    # se crean metodos normales de la clase
-    
-    def tomar_datos(self):
-        self.nombre_cliente = input("digite el nombre  : ")
-        self.apellido_cliente = input("digite el apellido : ")
-    
-    
-    def procesar_datos(self):
-        aux = self.nombre_cliente + " " + self.apellido_cliente
-    
-    def mostrar_info_cliente(self):
-        print(f"nombre cliente: {self.nombre_cliente} - apellido cliente: {self.apellido_cliente}")
-        
-    def hacer_saludo(self, datoSaludo):
-        print(f"{datoSaludo} : {self.nombre_cliente} {self.apellido_cliente}")
-    
+from abc import ABC, abstractmethod from datetime import datetime 
+class SerVivo(ABC): def init(self, nombre: str, edad: int): self._nombre = nombre self._edad = 
+edad 
+@abstractmethod 
+def alimentar(self): 
+    pass 
+ 
+def get_nombre(self): 
+    return self._nombre 
+ 
+def get_edad(self): 
+    return self._edad 
